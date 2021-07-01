eb284065e6dfb54b485af13e1f8a0886cb848e3e
root <root@localhost.localdomain>
2021-07-01 18:11:24 +0430

    new change

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -18,6 +18,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  * GameController.java
  * Controller to navigate the buttons on the game pages
  * new commit on github-testing 1
+ * testing poll
  */
 @Controller
 @RequestMapping("/game")
